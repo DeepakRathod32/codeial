dae0a8de9cf3d880312549f376cbf08c35202539
deepakrathod32 <deepakrathod7132@gmail.com>
2022-01-02 12:30:49 +0530

    static files are added in the project

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app =express();
+const app = express();
 const port = 8000;
+
+app.use(express.static('assets'));
 
 //use express router
 app.use('/', require('./routes'));
+
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
